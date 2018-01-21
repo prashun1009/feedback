733d53974301ff57c9a9a3972b65b1df97b91924
prashundwivedi <prashun1009@gmail.com>
2018-01-21 19:06:27 +0530

    valdation implimentation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,11 @@
 class Feedback < ApplicationRecord
+	validates :name, presence: true, length: { maximum: 20 }
+	validates :surname, presence: true
+	validates :description, presence: true
+	validates_length_of :description, minimum: 1, maximum:250, too_long: 'Your description must be below or equal to 250 words.'
+    private
+
+    def words_in_description
+      description.scan(/\w+/)
+    end
 end
