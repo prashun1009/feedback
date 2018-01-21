1e552bba3eb33d9be004910ff26e11f60e60388d
prashundwivedi <prashun1009@gmail.com>
2018-01-22 00:44:28 +0530

    modified after sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+    def after_sign_in_path_for(resource)
+       new_feedback_path
+    end 
+
+  	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation])
+  	end
 end
