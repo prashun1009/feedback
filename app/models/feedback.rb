9d6b9d47cb51f00c7ee0dc3c561f391fa7f9d9fe
prashundwivedi <prashun1009@gmail.com>
2018-01-21 16:26:57 +0530

    create home page

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,12 +1,2 @@
 class Feedback < ApplicationRecord
-	validates :name, presence: true, length: { maximum: 20 }
-	validates :description, presence: true
-	validates :surname, presence: true
-	validates :hobby, presence: true
-	validates_length_of :description, minimum: 1, maximum:250, too_long: 'Your description must be below or equal to 250 words.'
-	private
-
-	def words_in_description
-	  description.scan(/\w+/)
-	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'feedback/new'
+  get 'feedback/new'
 
   # post 'feedback/create'
   resources :feedbacks
