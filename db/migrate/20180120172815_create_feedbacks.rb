82825b64fda4f65d4c852be53316901e7e169f59
prashundwivedi <prashun1009@gmail.com>
2018-01-21 17:41:33 +0530

    migrating

--- db/migrate/20180120172815_create_feedbacks.rb
+++ db/migrate/20180120172815_create_feedbacks.rb
@@ -5,10 +5,8 @@ class CreateFeedbacks < ActiveRecord::Migration[5.1]
 		t.string :surname
 		t.string :hobby
 		t.text :description
-		t.references :user, foreign_key: true
 
 		t.timestamps
 	end
-	add_index :feedbacks, [:user_id, :created_at]
   end
 end
